4912bcd7f103208bbf9fe2d15001b88738683e54
HaseebullahMiakhil <tw773029@gmail.com>
2024-03-07 12:51:02 +0430

    database problem

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 
 app.listen(3000, () =>{
@@ -6,5 +7,13 @@ app.listen(3000, () =>{
 });
 
 app.get('/' ,(reg, res) => {
-    res.send("hello form node API");
+    res.send("hello from Node API server Updated");
+});
+
+mongoose.connect("mongodb+srv://tw773029:Trader123@@@backend.hqs2zcs.mongodb.net/?retryWrites=true&w=majority&appName=Backend")
+.then(() =>{
+    console.log("connected to the database!");
+})
+.catch(() =>{
+    console.log("its not connected to the database!");
 });
